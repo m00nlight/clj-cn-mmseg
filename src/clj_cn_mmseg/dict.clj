852d4fc3324911e0d6cf708a34edb7374ddb3832
m00nlight <dot_wangyushi@yeah.net>
2015-01-14 22:15:41 +0800

    Add load dictionary module

---
+++ src/clj_cn_mmseg/dict.clj
@@ -0,0 +1,169 @@
+;;;; file src/clj_cn_mmseg/dict.clj
+
+;;; This file contains related functions with load the multiple dictionary
+
+(ns clj-cn-mmseg.dict
+  (require [clojure.string :as s]
+           [clojure.set :as set]
+           [clojure.java.io :as io]
+           [clj-cn-mmseg.trie :as trie]))
+
+
+(def natures-map
+  {"名" :n, "动" :v, "形" :adj, "介" :prep, "叹" :inter,
+   "代" :pron, "副" :adv, })
+
+(defn word->natures
+  "Generate the word natures from the Chinese dictionary."
+  [word-and-natures]
+  (let [[word natures] (s/split (s/trim word-and-natures) #"\s+")]
+    {:word word
+     :nature (into {} (map #(into [] [(get natures-map % :not-know) true])
+                           (s/split natures #"/")))}))
+
+
+(defn wiki-concept->phrase
+  "Get the phrase representation of wiki concept. Wiki use underscore(_) to 
+seperate the word in concept phrase, Country_data_Angola for example. And also
+parentheses for disambiguation, like Ad_Lib_(字体). So the function will map 
+wiki's concept token to the phrase representation."
+  [concept]
+  (let [words (s/split (-> concept s/trim s/lower-case) #"[_\s]")]
+    (if (or (.endsWith (last words) ")")
+            (.endsWith (last words) "）"))
+      (interpose " " (drop-last words))
+      (interpose " " words))))
+
+
+(defn netlang->meaning
+  "Map the network language to their meaningful statement. The network 
+languages are stored under the resource/wikipedia_zh_cn.netlang, in the
+form AAA/BB/CC <--> DD/EE/FF. "
+  [pair-str]
+  (let [tmp (s/split (s/trim pair-str) #"<-->")
+        words (s/split (s/trim (first tmp)) #"/")]
+    (map (fn [x] {:word x :nature {:meaning (-> tmp second s/trim)}}) words)))
+
+(defn wikiper->per
+  "Change wikipedia per information, like drop disambiguation information,
+split of foreign names into chunks etc."
+  [name-str]
+  (let [names (s/split (s/trim name-str) #"·")]
+    (if (= (count names) 1)
+      (cons name-str '())
+      (cons name-str (filter #(>= (count %) 2) names)))))
+
+(defn wikiname->namelist
+  "Generate a name list from the name entity of wiki, since it may use mid dot
+in English names when expressed in Chinese. For example, 理查·泰勒 will generate
+'(\"理查·泰勒\", \"理查\", \"泰勒\")"
+  [name-str]
+  (let [names (s/split (s/trim name-str) #"·")]
+    (if (= (count names) 1)
+      names
+      (cons name-str (filter #(>= (count %) 2) names)))))
+
+
+(defn wikiname->redirect
+  "Redirect to the wiki concept of the origin meaning, now only do for 
+personal name, 习大大 -> 习近平."
+  [name-str]
+  (let [[nick, name] (s/split (s/trim name-str) #"<-->")]
+    (cons {:word nick :nature {:per true :redirect name}}
+          (map (fn [x] {:word x :nature {:per true}})
+               (rest (wikiname->namelist nick))))))
+
+(defn wikinick->redirect
+  [name-str type]
+  (let [[nick ,name ] (s/split (s/trim name-str) #"<-->")]
+    {:word nick, :nature {type true, :redirect name}}))
+
+(defn- load-dict-file
+  "Help function for load dictionary file."
+  [fname line-fn]
+  (let [file (io/file (io/resource fname))]
+    (map line-fn (s/split (slurp file) #"\r*\n"))))
+
+(defn concurrency->map
+  "Map between concurrency symbol and name. The form should be
+in the format N<-->S1/S2/S3, which means concurrency with name
+N can be used as symbol S1, S2 or S3."
+  [concurrency-str]
+  (let [[name tmp] (s/split (s/trim concurrency-str) #"<-->")
+        symbols (if-not (nil? tmp) (s/split tmp #"/") '())]
+    (cons {:word name
+           :nature {:concurrency true :meaning name}}
+          (map (fn [x] {:word x :nature {:concurrency true, :meaning name}})
+               symbols))))
+
+(defn- load-splitonly-dict
+  [fname]
+  (load-dict-file fname identity))
+
+(defn- load-feature-dict
+  [fname]
+  (load-dict-file fname word->natures))
+
+(defn- load-type-dict
+  "Type is an keyword indicate the type of the dictionary."
+  [fname type]
+  (load-dict-file fname (fn [x] {:word x :nature {type true}})))
+
+(defn- load-person-name-dict
+  [fname]
+  (map (fn [x] {:word x :nature {:per true}})
+       (flatten (load-dict-file fname wikiper->per))))
+
+(defn- load-person-redirect
+  [fname]
+  (flatten (load-dict-file fname wikiname->redirect)))
+
+(defn- load-redirect
+  [fname type]
+  (flatten (load-dict-file fname (fn [x] (wikinick->redirect x type)))))
+
+(defn- load-location-name-dict
+  [fname]
+  (load-type-dict fname :loc))
+
+(defn- load-idiom-dict
+  [fname]
+  (load-type-dict fname :idiom))
+
+(defn- load-netlang-dict
+  [fname]
+  (flatten (load-dict-file fname netlang->meaning)))
+
+(defn- load-concurrency-dict
+  [fname]
+  (flatten (load-dict-file fname concurrency->map)))
+
+
+(defn load-dicts
+  "Load dicts under the resource folder and build the trie for segment. 
+It only take an which is a function, if f is identity, means build the 
+trie in prefix style, if it is reverse, means build the trie in suffix
+style. The two different trie can do longest-prefix matching segment and
+longest-suffix matching segment correspondingly. Default is in the prefix
+manner."
+  ([] (load-dicts identity))
+  ([f]
+     (let [loc (load-location-name-dict "sogou.loc")
+           feature (load-feature-dict "xiandai_feature_lexicon.dict")
+           idiom (concat (load-idiom-dict "sogou_idioms.dict")
+                         (load-idiom-dict "xiandai_idiom.dict")) 
+           seg-only (concat (load-splitonly-dict "xiandai_segment_only.dict")
+                            (load-splitonly-dict "words.dict"))
+           nums (load-type-dict "num.dict" :num)
+           units (load-type-dict "all-unit.dict" :unit)
+           other-name (load-type-dict "other_name.dict" :name)
+           concurrency (load-concurrency-dict "concurrencies.dict")]
+       (-> {}
+           (trie/build-trie feature f)
+           (trie/build-trie loc f)
+           (trie/build-trie idiom f)
+           (trie/build-trie nums f)
+           (trie/build-trie units f)
+           (trie/build-trie concurrency f)
+           (trie/build-trie other-name f)
+           (trie/build-trie seg-only f)))))
---
+++ test/clj_cn_mmseg/dict_test.clj
@@ -0,0 +1,125 @@
+(ns clj-cn-mmseg.dict-test
+  (:require [clojure.test :refer :all]
+            [clj-cn-mmseg.dict :refer :all]
+            [clojure.string :as s]))
+
+(deftest test-word->natures
+  (testing "testing words with one nature."
+    (is (= {:word "哀乐", :nature {:n true}}
+           (word->natures "哀乐    名"))))
+  (testing "testing words with multiple natures."
+    (is (= {:word "挨", :nature {:v true, :prep true}}
+           (word->natures "挨      动/介")))
+    (is (= {:word "安", :nature {:adj true, :v true, :pron true}}
+           (word->natures "安      形/动/代"))))
+  (testing "testing words with new line at the end, should trim newline"
+    (is (= {:word "挨", :nature {:v true, :prep true}}
+           (word->natures "挨      动/介\n")))))
+
+
+
+(deftest test-wiki-concept->phrase
+  (testing "Testing for english wiki concepts without disambuity"
+    (let [res (wiki-concept->phrase "Country_data_Gambia")]
+      ;; length should be right
+      (is (= 5 (count res)))
+      ;; should all be lowercase word
+      (is (every? #(= (s/lower-case %) %) res))))
+  (testing "Testing concept with disambiguations."
+    (let [res (wiki-concept->phrase "Ace_(塔罗牌)")]
+      (is (= 1 (count res)))
+      (is (= "ace" (first res)))))
+  (testing "Testing wiki concept which have multiple space(_ in concept)"
+    (let [res (wiki-concept->phrase "J._A._哈普")]
+      (is (= 5 (count res)))
+      (is (= "j." (first res)))
+      (is (= "a." (nth res 2)))))
+  (testing "Testing concept with space in it and disambiguations."
+    (let [res (wiki-concept->phrase "亨利三世 (神圣罗马帝国)")
+          res2 (wiki-concept->phrase "理查·泰勒 (數學家)")]
+      (is (= 1 (count res)))
+      (is (= "亨利三世" (first res)))
+      (is (= 1 (count res2)))
+      (is (= "理查·泰勒" (first res2))))))
+
+(deftest test-wikiname->namelist
+  (testing "Testing for normal Chinese name without mid dot "
+    (is (= '("亨利三世")
+           (wikiname->namelist "亨利三世")))
+    (is (= '("Angelababy")
+           (wikiname->namelist "Angelababy"))))
+  (testing "Testing for English name with mid dot in it."
+    (is (= '("理查·泰勒" "理查" "泰勒")
+           (wikiname->namelist "理查·泰勒")))))
+
+(deftest test-netlang->meaning
+  (testing "Testing for one to one map."
+    (is (= '({:word "LZSB", :nature {:meaning "楼主傻逼"}})
+           (netlang->meaning "LZSB <--> 楼主傻逼")))
+    (is (= '({:word "深井冰", :nature {:meaning "神经病"}})
+           (netlang->meaning "深井冰 <--> 神经病")))
+    (is (= '({:word "PPMM", :nature {:meaning "漂亮的美眉/婆婆妈妈"}})
+           (netlang->meaning "PPMM <--> 漂亮的美眉/婆婆妈妈"))))
+  (testing "Testing for multiple to one map."
+    (is (= '({:word "民煮", :nature {:meaning "民主"}}
+             {:word "民猪", :nature {:meaning "民主"}}
+             {:word "皿煮", :nature {:meaning "民主"}}
+             {:word "免煮", :nature {:meaning "民主"}})
+           (netlang->meaning "民煮/民猪/皿煮/免煮 <--> 民主")))))
+
+
+(deftest test-wikiname->redirect
+  (testing "Tesing for Chinese name redirect."
+    (is (= '({:word "习大大" :nature {:per true :redirect "习近平"}})
+           (wikiname->redirect "习大大<-->习近平"))))
+  (testing "Tesing English name with space in it."
+    (is (= '({:word "Angela Baby" :nature {:per true :redirect "Angelababy"}})
+           (wikiname->redirect "Angela Baby<-->Angelababy"))))
+  (testing "Tesing English name with mid dot in it."
+    (is (= '({:word "托马斯·拉尼尔·威廉斯"
+              :nature {:per true :redirect "田纳西·威廉斯"}}
+             {:word "托马斯" :nature {:per true}}
+             {:word "拉尼尔" :nature {:per true}}
+             {:word "威廉斯" :nature {:per true}})
+           (wikiname->redirect "托马斯·拉尼尔·威廉斯<-->田纳西·威廉斯"))))
+  (testing "Tesing English name with mid dot and one character in it."
+    (is (= '({:word "卡尔·A·佩特里"
+              :nature {:per true :redirect "卡尔·亚当·佩特里"}}
+             {:word "卡尔" :nature {:per true}}
+             {:word "佩特里" :nature {:per true}})
+           (wikiname->redirect "卡尔·A·佩特里<-->卡尔·亚当·佩特里")))))
+
+(deftest test-wikiper->per
+  (testing "Testing for non-English names with no disambiguations."
+    (is (= '("徐昕")
+           (wikiper->per "徐昕")))
+    (is (= '("佐天仁皇后")
+           (wikiper->per "佐天仁皇后"))))
+  (testing "Testing for English names with no disambiguations."
+    (is (= '("西斯·格瑞辛格" "西斯" "格瑞辛格")
+           (wikiper->per "西斯·格瑞辛格")))
+    ;; should drop one characters in English name in result
+    (is (= '("皮埃尔·德·顾拜旦" "皮埃尔" "顾拜旦")
+           (wikiper->per "皮埃尔·德·顾拜旦"))))
+  (testing "Testing for name with space in it."
+    (is (= '("50 Cent")
+           (wikiper->per "50 Cent")))))
+
+
+(deftest test-concurrency->map
+  (testing "Tesing for mapping concurrency symbols."
+    (is (= '({:word "人民币" :nature {:concurrency true
+                                      :meaning "人民币"}}
+             {:word "¥" :nature {:concurrency true :meaning "人民币"}}
+             {:word "RMB" :nature {:concurrency true :meaning "人民币"}})
+           (concurrency->map "人民币<-->¥/RMB"))))
+  (testing "Testing for mapping concurrency with only one symbol."
+    (is (= '({:word "克朗" :nature {:concurrency true
+                                    :meaning "克朗"}}
+             {:word "Kr" :nature {:concurrency true :meaning "克朗"}})
+           (concurrency->map "克朗<-->Kr"))))
+  (testing "Testing for mapping concurrency with no symbol."
+    (is (= '({:word "卢比" :nature {:concurrency true
+                                    :meaning "卢比"}})
+           (concurrency->map "卢比")))))
+
