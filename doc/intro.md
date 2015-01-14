3a89b56129af21047f854753193dbaab8d40224b
m00nlight <dot_wangyushi@yeah.net>
2015-01-14 21:33:04 +0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-cn-mmseg
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_cn_mmseg/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-cn-mmseg.core-test
+  (:require [clojure.test :refer :all]
+            [clj-cn-mmseg.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
